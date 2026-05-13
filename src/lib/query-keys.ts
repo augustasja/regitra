9f6bf901d029bef187f4ed8838255119ed9fe30d
Augustas Jakaitis <jakaitisaugustas@gmail.com>
2026-05-13 18:26:13 +0300

    chore: "react query added"

---
+++ src/lib/query-keys.ts
@@ -0,0 +1,3 @@
+export const GET_VECHICLE_LIST = ["get-vehicle-list"];
+export const GET_VECHICLE_INFO = ["get-vehicle-info"];
+export const DELETE_VEHICLE = ["delete-vehicle"];
--- src/main.tsx
+++ src/main.tsx
@@ -4,12 +4,17 @@ import "./index.css";
 import App from "./App.tsx";
 import { StyledEngineProvider } from "@mui/material/styles";
 import GlobalStyles from "@mui/material/GlobalStyles";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <StyledEngineProvider enableCssLayer>
       <GlobalStyles styles="@layer theme, base, mui, components, utilities;" />
-      <App />
+      <QueryClientProvider client={queryClient}>
+        <App />
+      </QueryClientProvider>
     </StyledEngineProvider>
   </StrictMode>,
 );
