4d28ce2be57e2ab04a4a954727e21d76945734bd
Augustas Jakaitis <jakaitisaugustas@gmail.com>
2026-05-14 14:38:53 +0300

    chore: "router dom added"

--- src/main.tsx
+++ src/main.tsx
@@ -6,6 +6,7 @@ import { StyledEngineProvider } from "@mui/material/styles";
 import GlobalStyles from "@mui/material/GlobalStyles";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { SnackbarProvider } from "notistack";
+import { BrowserRouter } from "react-router-dom";
 
 const queryClient = new QueryClient();
 
@@ -13,11 +14,13 @@ createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <StyledEngineProvider enableCssLayer>
       <GlobalStyles styles="@layer theme, base, mui, components, utilities;" />
-      <QueryClientProvider client={queryClient}>
-        <SnackbarProvider autoHideDuration={1000}>
-          <App />
-        </SnackbarProvider>
-      </QueryClientProvider>
+      <BrowserRouter>
+        <QueryClientProvider client={queryClient}>
+          <SnackbarProvider autoHideDuration={1000}>
+            <App />
+          </SnackbarProvider>
+        </QueryClientProvider>
+      </BrowserRouter>
     </StyledEngineProvider>
   </StrictMode>,
 );
